99f008df410196940c34a387c8f17e7eca8eed82
psycho7 <zzccjj1234@gmail.com>
2015-04-12 22:07:58 +0800

    Update

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+class HomeController extends BaseController {
+	
+	public function home() {
+		echo "<h1>Nice!</h1>";
+	}
+
+}
+
+?>
--- config/routes.php
+++ config/routes.php
@@ -1,16 +1,9 @@
 <?php
 	use  \NoahBuscher\Macaw\Macaw;
 
-	// Test
-	Macaw::get('/hello', function() {
-		echo "Hello MiniFramework!";
-	});
-
-	Macaw::get('(:all)', function($fu) {
-		echo "Unable to route<br>".$fu;
-	});
-	// End Test
+	Macaw::get('/', 'HomeController@home');
 
 	Macaw::dispatch();
 
 ?>
+
